fa47f5a1332e8c86745754a279ab38beb5d17fb2
Fernando Iemini <ieminifernando@gmail.com>
2024-08-29 14:30:50 -0300

    First commit

---
+++ src/main/java/com/iemini/fernando/hacktown_web_app/HacktownWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.iemini.fernando.hacktown_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HacktownWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HacktownWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iemini/fernando/hacktown_web_app/HacktownWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iemini.fernando.hacktown_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HacktownWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
